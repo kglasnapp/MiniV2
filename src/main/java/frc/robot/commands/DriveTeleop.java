260af9608aa5094be45e636f9198c6d9751bf8e1
Keith Glasnapp <kglasnapp@gmail.com>
2023-05-13 17:24:40 -0700

    Test

--- src/main/java/frc/robot/commands/DriveTeleop.java
+++ src/main/java/frc/robot/commands/DriveTeleop.java
@@ -3,7 +3,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
-//import frc.robot.commands.DriveStraight.DriveMode;
 import frc.robot.subsystems.Drive;
 import frc.robot.utilities.PIDController;
 import frc.robot.utilities.RunningAverage;
